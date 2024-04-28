39cc607423e7ec9b4717772a447cac69edf3bbe6
ritakura <77808521+ritakura@users.noreply.github.com>
2024-04-28 08:13:10 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+EMAIL = "[gmail/google account of user who owns volunteer timesheet]"
+PASS = "[app password for google account goes here]"
+
+JSON = "[JSON file of keys for service account]"
+SHEETS_ID = "[The ID of the spreadsheets]"
